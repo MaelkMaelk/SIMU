13916ff3b3151bad81c8f1961b9552d4947ecdee
MaelkMaelk <65122264+MaelkMaelk@users.noreply.github.com>
2024-02-26 11:34:08 +0100

    install script

--- Client_Launch.sh
+++ Client_Launch.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-python -m pip install pygame
-python -m pip install pygame_gui
 python Python/client.py
---
+++ Install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python -m pip install pygame
+python -m pip install pygame_gui
